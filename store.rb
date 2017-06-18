c78df4f939ec502d37a49f2409b1c893dbb060fe
jux071 <jux071@gmail.com>
2017-06-18 10:46:02 +0200

    Uredjen store.rb fajl

--- store.rb
+++ store.rb
@@ -1,15 +1,15 @@
 class Store
-	attr_accessor :name, :street, :website, :phones
+  attr_accessor :name, :street, :website, :phones
 
-	def initialize (name, street, website, phones)
-	  @name = name
-	  @street = street
-	  @website = website	
-	  @phones = phones
-	end
+  def initialize (name, street, website, phones)
+    @name = name
+    @street = street
+    @website = website
+    @phones = phones
+  end
 
-	def to_s
-	  "************************************\nName: #{name} \nStreet: #{street} \nWebsite: #{website} \nTotal Phones: #{phones}"	
-	end
+  def to_s
+    "************************************\nName: #{name} \nStreet: #{street} \nWebsite: #{website} \nTotal Phones: #{phones}"
+  end
 
 end
